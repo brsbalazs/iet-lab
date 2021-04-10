82db0c69bf8b52c9e8dbf341f4e69697bd5b6f45
Béres Balázs <beres.balazs@innobyte.hu>
2021-04-10 14:29:07 +0200

    Update comments

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,7 +77,7 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
-        // try to fire both of the torpedo stores
+        // try to fire both of the torpedo stores if stores are not empty
 
         //initially no torpedos
         if(secondaryTorpedoStore.isEmpty() && primaryTorpedoStore.isEmpty()){
